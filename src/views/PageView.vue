be35c41355dcbef431a05293fe7c3b45afe70beb
甜甜 <nbios@qq.com>
2026-09-13 15:30:13 +0800

    feat: 推送views和页面文件

---
+++ src/views/PageView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ZCContainer">
+    <div class="ZCLayout">
+      <div class="ZCContent">
+        <slot name="doc-before"></slot>
+        <template v-if="posts">
+          <PostList :posts="posts" />
+          <Pagination :pagination="pagination" :total="total" :homepage="homepage" />
+        </template>
+        <slot name="doc-after"></slot>
+      </div>
+      <aside class="ZCAside ZCAside--left"><slot name="aside-left"></slot></aside>
+      <aside class="ZCAside ZCAside--right"><slot name="aside-right"></slot></aside>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useData } from 'vitepress';
+import PostList from '../components/PostList.vue';
+import Pagination from '../components/Pagination.vue';
+import type { IPost } from '../types';
+const props = defineProps({
+  pagination: { type: Number, required: true },
+  total: { type: Number, required: true },
+  size: { type: Number, required: true },
+  homepage: Boolean
+});
+const { theme } = useData();
+const posts: IPost[] = (theme.value.posts ? [...theme.value.posts] : []).slice(props.size * (props.pagination - 1), props.size * props.pagination);
+</script>
+
+<style lang="less" scoped>
+@import '../styles/page.less';
+</style>
