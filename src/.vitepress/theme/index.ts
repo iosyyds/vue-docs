e0bf7057daaf5756af787e921023d4c99bcedb72
甜甜 <nbios@qq.com>
2026-09-13 14:04:06 +0800

    feat: 在文章标题下方注入元信息行

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import Giscus from './Giscus.vue'
+import PostMeta from './PostMeta.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(PostMeta),
       'doc-after': () => h(Giscus)
     })
   },
