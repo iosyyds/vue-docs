12c320cc22ea5106ab06f80f798486f2929cbe01
甜甜 <nbios@qq.com>
2026-09-14 20:05:32 +0800

    feat: Tags

---
+++ .vitepress/theme/components/Tags.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <h1 class="page-title">文章标签</h1>
+    <div class="tag-cloud">
+      <span v-for="t in tags" :key="t.name" class="tag-pill"
+            :style="{ fontSize: Math.min(0.9 + t.count * 0.1, 1.5) + 'em' }">
+        {{ t.name }}<em>{{ t.count }}</em>
+      </span>
+    </div>
+    <p v-if="!tags.length" class="empty">暂无标签</p>
+  </div>
+</template>
+<script setup>
+import { data as posts } from '../../posts.data.mts'
+const tags = (() => {
+  const m = {}
+  posts.forEach(p => { (p.tags || []).forEach(t => { m[t] = (m[t]||0) + 1 }) })
+  return Object.entries(m).map(([name, count]) => ({ name, count }))
+})()
+</script>
